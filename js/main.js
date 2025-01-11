0d06b03d99b77d0eda10676ddd5127b5270e2c10
beast1309 <56633284+beast1309@users.noreply.github.com>
2025-01-12 03:50:37 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -30,8 +30,78 @@ $(function () {
         $('.waters__map-inner').toggleClass('--hidden')
     })
 
+    $('#formul-btn').on('click', function () {
+        $('#calc-btn').removeClass('--acitve')
+        $('#calc-btn').removeAttr('disabled')
+        $('#formul-btn').toggleClass('--active')
+        $('#formul-btn').attr('disabled', 'disabled')
+        $('.header__nav').removeClass('--calc')
+        $('.header__nav').toggleClass('--formul')
+        $('.calc__formul-inner').removeClass('--hidden')
+        $('.calc__calc-inner').toggleClass('--hidden')
+    })
+
+    $('#calc-btn').on('click', function () {
+        $('#formul-btn').removeClass('--acitve')
+        $('#formul-btn').removeAttr('disabled')
+        $('#calc-btn').toggleClass('--active')
+        $('#calc-btn').attr('disabled', 'disabled')
+        $('.header__nav').removeClass('--formul')
+        $('.header__nav').toggleClass('--calc')
+        $('.calc__calc-inner').removeClass('--hidden')
+        $('.calc__formul-inner').toggleClass('--hidden')
+    })
+
     $('#type').on('click', function () {
         $(document.querySelectorAll('#typeSort')).toggleClass('--active')
+    })
+
+    const type = document.getElementById('type')
+    const typeSort = document.querySelectorAll('#typeSort')
+    const watersTable = document.querySelectorAll('.waters__table-body')
+    const watersTableRow = document.querySelectorAll('#waters-table-row')
+    const watersTableCell = document.querySelectorAll('#waters-table-type')
+    let rowArr = Array(watersTableRow)
+    typeSort.forEach((item) => {
+        item.addEventListener('click', function () {
+            if (item.value == 0) console.log('0')
+
+            else if (item.value == 2) {
+                watersTableCell.forEach((cell) => {
+                    cell.value = cell.innerHTML
+                    watersTableRow.forEach((row) => {
+                        if (row.contains(cell) && cell.value == 'Гидрант') {
+                            row.parentNode.removeChild(row)
+                            console.log(watersTable)
+                            console.log(rowArr)
+                            console.log(row)
+                        }
+                    })
+                })
+
+
+
+                /* if (watersTableCell.innerText == 'Гидрант'.toLowerCase()) console.log('1')
+                watersTableRow.forEach((row) => {
+                    console.log(row)
+                    console.log(watersTableCell)
+                }) */
+            }
+
+            else if (item.value == 1) {
+                watersTableCell.forEach((cell) => {
+                    cell.value = cell.innerHTML
+                    watersTableRow.forEach((row) => {
+                        if (row.contains(cell) && cell.value == 'Водоём') {
+                            row.parentNode.removeChild(row)
+                            console.log(row.parentNode)
+                        }
+                    })
+                })
+            }
+
+            $(typeSort).removeClass('--active')
+        })
     })
 
     let myMap;
@@ -116,5 +186,122 @@ $(function () {
             windText.innerHTML = (windResponse.current.wind_kph * 1000 / 3600).toFixed(2) + 'м/с'
         })
 
+    $('.calculator__btn').on('click', function calc() {
 
+        let firstFM = document.getElementById('firstFM').value
+        let secondFM = document.getElementById('secondFM').value
+        let therdFM = document.getElementById('therdFM').value
+        let fourFM = document.getElementById('fourFM').value
+        let fiveFM = document.getElementById('fiveFM').value
+
+        let HoursOn = document.getElementById('HoursOn').value
+        let MinutesOn = document.getElementById('MinutesOn').value
+
+        let AirQ = document.getElementById('AirQ').value
+        let BallonV = document.getElementById('BallonV').value
+
+        let FastPmin__on = document.getElementById('FastPmin__on').value
+
+        let Pmin__on = [firstFM, secondFM, therdFM, fourFM, fiveFM]
+
+        Array.prototype.remove = function (value) {
+            var index = this.indexOf(value)
+            if (index !== -1) {
+                this.splice(index, 1)
+            }
+        }
+
+        for (i = 0; i < 5; i++) {
+
+            const zeroValue = 0
+            const emptyValue = ''
+            Pmin__on.remove(zeroValue)
+            Pmin__on.remove(emptyValue)
+        }
+
+        let min = Math.min(...Pmin__on)
+
+        FastPmin__on = min
+
+        let Pmax__pad = min / 3
+        let Pcontrexit = min - Pmax__pad
+        let DeltaTime = (Pmax__pad * BallonV) / AirQ
+
+
+        HoursOn = parseInt(HoursOn)
+        MinutesOn = parseInt(MinutesOn)
+
+
+
+        if ((MinutesOn + DeltaTime) < 59) {
+
+            MinutesOn += DeltaTime
+        }
+
+        else if ((MinutesOn + DeltaTime) > 59) {
+
+            HoursOn += 1;
+            MinutesOn = (MinutesOn + DeltaTime) - 60
+
+        }
+
+        if (HoursOn < 10) {
+            HoursOn = '0' + HoursOn
+        }
+
+        if (HoursOn > 23) {
+            HoursOn = '0' + 0
+        }
+
+        let Texit;
+
+        if (MinutesOn < 10) {
+            Texit = HoursOn + ' : 0' + Math.floor(MinutesOn)
+        }
+
+        else if (MinutesOn > 10) {
+            Texit = HoursOn + ' : ' + Math.floor(MinutesOn)
+        }
+
+        let Tjob = (min * BallonV) / AirQ
+
+        let Tback
+
+        HoursOn = parseInt(HoursOn)
+        MinutesOn = parseInt(MinutesOn)
+
+        if ((MinutesOn + Tjob) < 59) {
+            MinutesOn += Tjob
+        }
+
+        else if ((MinutesOn + Tjob) > 59) {
+            HoursOn += 1
+            MinutesOn = (MinutesOn + Tjob) - 60
+
+        }
+
+        if (HoursOn < 10) {
+            HoursOn = '0' + HoursOn
+        }
+
+        if (HoursOn > 23) {
+            HoursOn = '0' + 0
+        }
+
+        if (MinutesOn < 10) {
+            Tback = HoursOn + ' : 0' + Math.floor(MinutesOn)
+        }
+
+        else if (MinutesOn > 10) {
+            Tback = HoursOn + ' : ' + Math.floor(MinutesOn)
+        }
+
+        document.getElementById('Pmax__pad').innerHTML = Math.floor(Pmax__pad)
+        document.getElementById('Pcontrexit').innerHTML = Math.floor(Pcontrexit)
+        document.getElementById('DeltaTime').innerHTML = Math.floor(DeltaTime)
+        document.getElementById('Texit').innerHTML = Texit
+        document.getElementById('Tjob').innerHTML = Math.floor(Tjob)
+        document.getElementById('Tback').innerHTML = Tback
+        document.getElementById('FastPmin__on').innerHTML = FastPmin__on
+    })
 });
